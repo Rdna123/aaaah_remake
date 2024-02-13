23454522ed3ec14988b37d8fa33b619b0519705e
Robert Kimura <chknmanrob1904@gmail.com>
2024-02-12 22:04:49 -0800

    Server Sync

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,26 @@
-use bevy::audio::PlaybackMode;
+use bevy::audio::{PlaybackMode, Volume};
 use bevy::prelude::*;
+use std::time::Duration;
 
 fn main() {
     App::new()
-        .add_plugins(DefaultPlugins.set(WindowPlugin{
-            primary_window: Some(Window{
+        .add_plugins(DefaultPlugins.set(WindowPlugin {
+            primary_window: Some(Window {
                 title: "AH".into(),
                 ..default()
             }),
             ..default()
         }))
         .add_systems(Startup, setup)
-        .add_systems(Update, (make_mark_scream,aah))
+        .add_systems(Update, (make_mark_scream, aah_window_title, screaming_face))
         .run();
 }
 
-fn aah(input: Res<Input<KeyCode>>,mut window: Query<&mut Window>){
-    if input.pressed(KeyCode::A){
+fn aah_window_title(input: Res<Input<KeyCode>>, mut window: Query<&mut Window>) {
+    if input.pressed(KeyCode::A) {
         let mut window = window.single_mut();
         let count = window.title.len();
         let mut title = "".to_string();
-
 
         for _ in 0..count {
             title += "A";
@@ -34,23 +34,80 @@ fn aah(input: Res<Input<KeyCode>>,mut window: Query<&mut Window>){
 
 #[derive(Component)]
 struct MarkScream;
+
+#[derive(Component, PartialEq)]
+enum State {
+    Stop,
+    Go,
+}
+
+#[derive(Component)]
+struct ScreamTimer {
+    timer: Timer,
+}
+
 fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
     commands.spawn((
         AudioBundle {
             source: asset_server.load("sound/markie-screaming.ogg"),
-            settings: PlaybackSettings{
+            settings: PlaybackSettings {
                 mode: PlaybackMode::Loop,
+                volume: Volume::new_relative(3.0),
                 ..default()
-            }
+            },
         },
         MarkScream,
     ));
+    commands.spawn((
+        SpriteBundle {
+            texture: asset_server.load("images/mark.png"),
+            transform: Transform {
+                translation: Vec3::new(100.0, 0.0, 1.0),
+                scale: Vec3::new(1.0, 1.0, 1.0),
+                ..default()
+            },
+            ..default()
+        },
+        State::Go,
+        ScreamTimer {
+            timer: Timer::new(Duration::from_secs_f32(6.59), TimerMode::Repeating),
+        },
+    ));
 }
 
-fn make_mark_scream(input: Res<Input<KeyCode>>,sound_controller: Query<&AudioSink, With<MarkScream>>){
-    if input.just_pressed(KeyCode::S){
-        if let Ok(sink) = sound_controller.get_single(){
+fn make_mark_scream(
+    input: Res<Input<KeyCode>>,
+    sound_controller: Query<&AudioSink, With<MarkScream>>,
+    mut scream: Query<(&mut State, &mut ScreamTimer)>,
+) {
+    if input.just_pressed(KeyCode::S) {
+        if let Ok(sink) = sound_controller.get_single() {
             sink.toggle();
+        }
+        if let Ok((mut s, mut t)) = scream.get_single_mut() {
+            match *s {
+                State::Stop => {
+                    *s = State::Go;
+                    t.timer.unpause()
+                }
+                State::Go => {
+                    *s = State::Stop;
+                    t.timer.pause()
+                }
+            }
+        }
+    }
+}
+
+fn screaming_face(mut image: Query<(&mut Transform, &State, &mut ScreamTimer)>, time: Res<Time>) {
+    for (mut transform, state, mut scream) in &mut image {
+        debug!("aaah");
+        if state == &State::Go {
+            transform.scale.x += 1.0;
+            transform.scale.y += 1.0;
+        }
+        if scream.timer.tick(time.delta()).just_finished() {
+            transform.scale = Vec3::ZERO;
         }
     }
 }
